b456cc8b09c2f7166fd1d39717086425dbcec618
KingdomCoder <kingdomcoding@gmail.com>
2025-10-21 17:38:27 +0100

    chore: log all socknames

--- lib/ex_ice/priv/transport/udp.ex
+++ lib/ex_ice/priv/transport/udp.ex
@@ -2,11 +2,19 @@ defmodule ExICE.Priv.Transport.UDP do
   @moduledoc false
   @behaviour ExICE.Priv.Transport
 
+  require Logger
+
   @impl true
   defdelegate open(port, opts), to: :gen_udp
 
   @impl true
-  defdelegate sockname(socket), to: :inet
+  def sockname(socket) do
+    socknames = :inet.socknames(socket)
+
+    Logger.debug("MY-DEBUG Socknames: #{inspect(socknames)}")
+
+    :inet.sockname(socket)
+  end
 
   @impl true
   defdelegate send(socket, dest, packet), to: :gen_udp
